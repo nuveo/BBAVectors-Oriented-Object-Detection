403d5b1fa1aa685454dd878d99f06ba8808ca84f
esgario <guilherme.esgario@gmail.com>
2021-07-16 18:40:21 -0300

    Update code to save results (#2)
    * wip: upate code to save results
    
    * Finish results modification

--- run-docker.sh
+++ run-docker.sh
@@ -9,13 +9,17 @@ weights_dir="${1}"
 image_name=$(basename -- "$2")
 image_dir="$( cd "$(dirname "$2")" >/dev/null 2>&1 ; pwd -P )"
 drone_altitude=$3
+results_path=$(pwd)/results
+
+mkdir -p $results_path
 
 docker run --rm -it --gpus=all \
 	-v "$image_dir":/image_dir/ \
 	-v "$weights_dir":/weights_dir/ \
+	-v "$results_path":/results/ \
 	bbavectors \
 	python ./bbavectors/app/object_detection.py \
 		--model_dir /weights_dir/"$weight_dir" \
 		--image /image_dir/"$image_name" \
-		--altitude $drone_altitude
+		--altitude $drone_altitude --plot
 
