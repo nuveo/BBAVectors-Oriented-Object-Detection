0e58f101f35291a8ab26a5c187527921b6ba7a84
Guilherme Esgario <guilherme.esgario@gmail.com>
2021-08-04 21:15:58 -0300

    Update arguments

--- bbavectors/app/main.py
+++ bbavectors/app/main.py
@@ -4,23 +4,38 @@ import argparse
 
 def parse_args():
     parser = argparse.ArgumentParser(description='BBAVectors')
-    parser.add_argument('weights_dir', type=str, help="weights directory")
-    parser.add_argument('image_path', type=str, help="image path")
-    parser.add_argument('drone_altitude', type=int, help="drone altitude")
+    parser.add_argument(
+        'env', type=str, help="Execution environment [docker/local]")
+    parser.add_argument('weights_dir', type=str, help="Weights directory")
+    parser.add_argument('image_path', type=str, help="Image path")
+    parser.add_argument('drone_altitude', type=int, help="Drone altitude")
     parser.add_argument('--plot', dest='plot', action='store_true')
     parser.add_argument('--cpu', dest='cpu', action='store_true')
-    parser.add_argument('--no-docker', dest='nodocker', action='store_true')
-    return parser.parse_args()
+    args = parser.parse_args()
+    if args.env != "docker" and args.env != "local":
+        parser.print_help()
+        exit(1)
+    return args
 
 
 def main():
     """BBAVectors Inference CLI.
 
-    Usage:
-        bbavectors [-h] weights_dir image_path drone_altitude [OPTIONS]
+    usage: bbavectors [-h] [--plot] [--cpu]
+                  env weights_dir image_path drone_altitude
 
-    Options:
-        -h, --help      Show this message.
+    BBAVectors
+
+    positional arguments:
+    env             Execution environment [docker/local]
+    weights_dir     Weights directory
+    image_path      Image path
+    drone_altitude  Drone altitude
+
+    optional arguments:
+    -h, --help      show this help message and exit
+    --plot
+    --cpu
     """
 
     args = parse_args()
@@ -31,24 +46,21 @@ def main():
     altitude = int(args.drone_altitude)
     results_path = os.getcwd()
 
-    if args.nodocker:
+    if args.env == "local":
         from bbavectors.app import run_inference
         run_inference(
             args.weights_dir, args.image_path,
             altitude, args.plot
         )
     else:
-        command = f"""
-            docker run --rm -it {use_gpu}
-            -v "{image_dir}":/image_dir/
-            -v "{args.weights_dir}":/weights_dir/
-            -v "{results_path}":/results/ \
-            bbavectors
-            bbavectors
-                /weights_dir/"{args.weights_dir}"
-                /image_dir/"{image_name}"
-                {altitude} --plot --no-docker
-        """
+        command = (
+            f'docker run --rm -it {use_gpu} '
+            f'-v "{image_dir}":/image_dir/ '
+            f'-v "{args.weights_dir}":/weights_dir/ '
+            f'-v "{results_path}":/results/ bbavectors '
+            f'bbavectors local /weights_dir/ /image_dir/"{image_name}" '
+            f'{altitude} --plot'
+        )
         os.system(command)
 
 
