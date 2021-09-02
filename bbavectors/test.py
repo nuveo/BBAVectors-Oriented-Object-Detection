a94f80038a550fa61a0ed709adbdf0e87703abe7
esgario <guilherme.esgario@gmail.com>
2021-09-02 10:49:47 -0300

    Update dataset class (#6)
    * Update datasets to automate detect categories
    
    * Remove breakpoint

--- bbavectors/test.py
+++ bbavectors/test.py
@@ -4,7 +4,8 @@ import cv2
 import time
 import os
 import matplotlib.pyplot as plt
-from bbavectors import ROOT
+from datasets.dataset import Dataset
+from bbavectors import WORK_DIR
 from . import func_utils
 
 
@@ -21,11 +22,10 @@ def apply_mask(image, mask, alpha=0.5):
 
 
 class TestModule(object):
-    def __init__(self, dataset, num_classes, model, decoder):
+    def __init__(self, num_classes, model, decoder):
         torch.manual_seed(317)
         self.device = torch.device(
             "cuda:0" if torch.cuda.is_available() else "cpu")
-        self.dataset = dataset
         self.num_classes = num_classes
         self.model = model
         self.decoder = decoder
@@ -89,12 +89,12 @@ class TestModule(object):
         self.model = self.model.to(self.device)
         self.model.eval()
 
-        dataset_module = self.dataset[args.dataset]
-        dsets = dataset_module(data_dir=args.data_dir,
-                               phase='test',
-                               input_h=args.input_h,
-                               input_w=args.input_w,
-                               down_ratio=down_ratio)
+        dsets = Dataset(data_dir=args.data_dir,
+                        phase='test',
+                        num_classes=args.num_classes,
+                        input_h=args.input_h,
+                        input_w=args.input_w,
+                        down_ratio=down_ratio)
         data_loader = torch.utils.data.DataLoader(dsets,
                                                   batch_size=1,
                                                   shuffle=False,
